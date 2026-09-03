3fa6f81bbbf79dbd56bb6d0893cf5687aa9e82d2
Rishabh3443 <risabhtri2478@gmail.com>
2026-09-03 13:47:11 +0530

    solving patterns

--- pattern.cpp
+++ pattern.cpp
@@ -1,16 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+void print1(int n)
+{
+   
+    for( int i =0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<"x";
+        }
+        cout<<endl;
+    }
+};
 
-   for(int i =1; i<=5; i++){
+void print2(int n){
 
-    for(int j = 1;j<5;j++){
-        cout<<"* ";
+   for( int i =1; i<n; i++){
+        for(int j=1; j<i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+};
+
+void print3(int n){
+
+   for(int i =n; i<n; i--){
+    for(int j = i; j<n; j++){
+        cout<<j;
     }
     cout<<endl;
-
    }
+}
+
+void print4(int n){
+
+}
+
+int main()
+{
+    int n;
+    cin >> n; 
+    print3(n);
 
     return 0;
 }
