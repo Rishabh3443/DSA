b73efc74bf8e3298bf5e09449717fd93ad5f0b30
Rishabh3443 <risabhtri2478@gmail.com>
2026-09-19 11:08:55 +0530

    create folders to make more structured

--- DSA-Practice/pattern.cpp
+++ DSA-Practice/pattern.cpp
@@ -66,12 +66,59 @@ void print5(int &n)
     }
 }
 
+void print6(int &n)
+{ 
+    for (int i = 0; i < n; i++){
+        
+        for(int j = 0; j<(n-i-1);j++ ){
+         
+            cout<<" ";
+        }
+        
+
+        for (int k = 0; k < (2*i+1); k++)
+        {
+            cout<<"x";
+        }
+
+        
+
+        
+        cout<<endl;
+
+        
+       
+    }
+    for (int i = 0; i < n; i++){
+        
+        for(int j = 0; j<i;j++ ){
+         
+            cout<<" ";
+        }
+        
+
+        for (int k = 0; k < (2*n-1-2*i); k++)
+        {
+            cout<<"x";
+             
+        }
+
+       
+
+        
+        cout<<endl;
+
+        
+       
+    }
+}
+
 
 int main()
 {
     int n = 5;
 
-    print5(n);
+    print6(n);
 
     return 0;
 }
